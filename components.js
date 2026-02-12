cb154ab88a7e75192e1afc0298a632fee5c4bba3
Zixuan Huang <zxgamingh@gmail.com>
2026-02-12 18:53:52 +0100

    Navbar and footer base components

---
+++ components.js
@@ -0,0 +1,39 @@
+class CustomNavbar extends HTMLElement {
+    connectedCallback() {//makes sure it runs
+        this.innerHTML = `
+            <nav>
+                <a href="test.html" id="home-button">Home</a>               
+                <ul>                           
+                    <li>
+                        <a href="" class="nav-button">About</a>
+                    </li>
+                    <li>
+                        <a href="" class="nav-button">Works</a>
+                    </li>
+                    <li>
+                        <a href="" class="nav-button">Tour</a>
+                    </li>
+                    <li>
+                        <a href="" class="nav-button">Merch</a>
+                    </li>                                 
+                    <li>
+                        <a href="" class="nav-button">Socials</a>
+                    </li>                                                                                                                 
+                </ul>
+            </nav>        
+        `
+    }
+};
+
+class CustomFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <span>This is a temporary footer</span>
+        </footer>
+        `
+    }
+};
+
+customElements.define("custom-navbar", CustomNavbar);
+customElements.define("custom-footer", CustomFooter);
