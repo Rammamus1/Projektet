fc3d0cece2ee97c3c88c567d02cb70b353c57d4e
Rammamus1 <adrian.folin@gmail.com>
2026-02-19 21:58:58 +0100

    mer mer style fix

--- AdrianZixuanMain/main.js
+++ AdrianZixuanMain/main.js
@@ -6,25 +6,28 @@ function scrollToArticle(id) {
 }
 
 
-const track = document.getElementById('carouselTrack');
-const slides = Array.from(track.children);
+const track = document.getElementById('carouselTrack'); //Hittar hela karusellen
+const slides = Array.from(track.children); //Hittar alla objekt i carusellen
 const nextButton = document.querySelector('.arrow.right');
 const prevButton = document.querySelector('.arrow.left');
 
 let currentIndex = 0;
 
 function updateCarousel() {
+    //EVIL
     const slideWidth = slides[0].getBoundingClientRect().width + parseFloat(getComputedStyle(track).gap);
-    track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    track.style.transform = `translateX(-${currentIndex * slideWidth}px)`; //EVEN MORE EVIL
 }
 
+//Klickar vi på höger knappen ska vi gå fram ett steg, funkar bara om det är möjligt att gå fram
 nextButton.addEventListener('click', () => {
-    if (currentIndex < slides.length - 3) { // 3 bilder visas
+    if (currentIndex < slides.length - 3) {
         currentIndex++;
         updateCarousel();
     }
 });
 
+//Klickar vi på vänster knappen går vi tillbaka, funkar bara om det är möjligt
 prevButton.addEventListener('click', () => {
     if (currentIndex > 0) {
         currentIndex--;
